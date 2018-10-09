876fddf1246cf7bf8a1eb6995dd8d0afb89dfa46
tdesosa <tdesosa@gmail.com>
2018-10-09 17:07:33 -0600

    index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -4,11 +4,28 @@ const port = 3000;
 
 const Pokemon = require('./models/pokemon');
 
-//create a get route /pokemon that will res.send(pokemon), which will display your pokemon data as json in the browser
+// Create a get route which will display your pokemon data as json in the browser
 
 app.get("/pokemon", (req, res) => {
   res.send(Pokemon);
 });
+
+// Set up your index view
+
+app.get("/pokemon/:index", (req, res) => {
+  const context = {
+    pokemon: Pokemon
+  }
+  res.render('index.ejs', context);
+});
+
+//Set up your show route
+
+
+
+
+// Server listening
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
