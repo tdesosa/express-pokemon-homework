78cc04cb07f271a96fd3c02b621d6bc54636e4fc
tdesosa <tdesosa@gmail.com>
2018-10-10 17:22:20 -0600

    post route set up

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const Pokemon = require('./models/pokemon');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
 // Create a get route which will display your pokemon data as json in the browser
 
-app.get("/pokemon", (req, res) => {
-  res.send(Pokemon);
-});
+// app.get("/pokemon", (req, res) => {
+//   res.send(Pokemon);
+// });
 
 // Set up your index view (Used Context Variable For Learning Purposes)
 
-app.get("/pokemon/index", (req, res) => {
+app.get("/pokemon", (req, res) => {
   const context = {
     pokemon: Pokemon
   }
   res.render('index.ejs', context);
+});
+
+// Set up a post route
+
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+
+app.post('/pokemon', (req, res) => {
+  Pokemon.push(req.body);
+  res.redirect('/pokemon');
 });
 
 //Set up your show route
@@ -27,9 +44,13 @@ app.get("/pokemon/:id", (req, res) => {
   });
 });
 
+
+
 // Server listening
 
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
+
+module.exports = app
